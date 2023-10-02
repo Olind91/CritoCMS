0d17ee14c992020bf2bf648a097c4e57bbb06039
oskar <Osklind91@hotmail.com>
2023-10-02 16:49:33 +0200

    Add project files.

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,42 @@
+﻿using MailKit;
+using Microsoft.AspNetCore.Mvc;
+using oskarCMS.Models;
+using oskarCMS.Services;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace oskarCMS.Controllers
+{
+    public class ContactsController : SurfaceController
+    {
+        public ContactsController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(ContactForm contactForm)
+        {
+           if(!ModelState.IsValid)
+                return CurrentUmbracoPage();
+
+            using var mail = new MailHandler("no-reply-limpan@internet.com", "smtp.support.se", 587, "umraco@test.com", "BytMig123!" );
+
+            //sender
+            mail.SendAsync(contactForm.Email, "Task was Successful", "hi, test").ConfigureAwait(false);
+
+
+            //us
+            mail.SendAsync("umraco@test.com", $"{contactForm.Name} sent a request", contactForm.Message).ConfigureAwait(false);
+
+            return LocalRedirect(contactForm.RedirectUrl ?? "/");
+
+            
+        }
+    }
+}
---
+++ Services/MailHandler.cs
@@ -0,0 +1,76 @@
+﻿using Lucene.Net.Queries.Function.DocValues;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+using System.Diagnostics;
+using System.Net.Mail;
+
+
+namespace oskarCMS.Services
+{
+    public class MailHandler : IDisposable
+    {
+        private string _from;
+        private string _smtp;
+        private int _port;
+        private string _username;
+        private string _password;
+        private MailKit.Net.Smtp.SmtpClient _smtpClient;
+
+        public MailHandler(string from, string smtp, int port, string username, string password)
+        {
+            _from = from;
+            _smtp = smtp;
+            _port = port;
+            _username = username;
+            _password = password;
+
+            _smtpClient = new MailKit.Net.Smtp.SmtpClient();
+        }
+
+        //forms & surface controllers-föreläsning. 43 min in
+        public async Task SendAsync(string to, string subject, string body)
+        {
+            try
+            {
+                var email = new MimeMessage();
+                email.From.Add(MailboxAddress.Parse(_from));
+                email.To.Add(MailboxAddress.Parse(to));
+                email.Subject = subject;
+                email.Body = new TextPart(TextFormat.Html) { Text = body };
+
+                
+                await _smtpClient.ConnectAsync(_smtp, _port, SecureSocketOptions.StartTls);
+                await _smtpClient.AuthenticateAsync(_username, _password);
+
+                var result = await _smtpClient.SendAsync(email);
+            }
+            catch (Exception ex) 
+            { 
+                Debug.WriteLine(ex.Message);
+            }
+        }
+        
+
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+
+
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+                _smtpClient.DisconnectAsync(true).ConfigureAwait(false);
+
+        }
+
+
+    }
+}
